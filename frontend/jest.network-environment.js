0db008773cf2b3aac25fd72d0524d94d23ad24ee
kojesung <js95112345@gmail.com>
2026-09-14 14:10:38 +0900

    test(frontend): MSW용 jest 네트워크 환경 추가
    cherry-pick 0b69e41 중 jest 설정만 가져온다. 함께 있던 ProjectsPage.test.tsx는
    대상 페이지가 이 브랜치에 없어 제외했다.

---
+++ frontend/jest.network-environment.js
@@ -0,0 +1,26 @@
+import { TestEnvironment } from 'jest-environment-jsdom';
+
+// MSW와 ky가 사용하는 표준 네트워크 API를 Node에서 가져온다.
+export default class NetworkEnvironment extends TestEnvironment {
+  async setup() {
+    await super.setup();
+    for (const name of [
+      'fetch',
+      'Headers',
+      'Request',
+      'Response',
+      'FormData',
+      'Blob',
+      'TextEncoder',
+      'TextDecoder',
+      'ReadableStream',
+      'WritableStream',
+      'TransformStream',
+      'BroadcastChannel',
+      'AbortController',
+      'AbortSignal',
+    ]) {
+      this.global[name] = globalThis[name];
+    }
+  }
+}
